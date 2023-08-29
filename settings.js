8d9a09e0166682877c1996f02af134e96383b584
fatmaboodai <76260193+fatmaboodai@users.noreply.github.com>
2023-08-30 01:21:38 +0300

    draft 30/8

--- settings.js
+++ settings.js
@@ -1,106 +1,5 @@
 
-// // with the tab select
-
-
-// const startRecordingButton = document.getElementById("startRecording");
-// const recordedVideoElement = document.getElementById("recordedVideo");
-// let mediaRecorder;
-// let recordedChunks = [];
-
-// async function startRecording() {
-//     try {
-//         const constraints = {
-//             video: {
-//                 displaySurface: 'browser',
-//                 logicalSurface: false,
-//                 mediaSource: 'window'
-//             },
-//             audio: false
-//         };
-
-//         const stream = await navigator.mediaDevices.getDisplayMedia(constraints);
-
-//         mediaRecorder = new MediaRecorder(stream);
-        
-//         mediaRecorder.ondataavailable = (event) => {
-//             if (event.data.size > 0) {
-//                 recordedChunks.push(event.data);
-//             }
-//         };
-
-//         mediaRecorder.onstop = () => {
-//             const recordedBlob = new Blob(recordedChunks, { type: 'video/webm' });
-//             recordedVideoElement.src = URL.createObjectURL(recordedBlob);
-//         };
-
-//         mediaRecorder.start();
-//         startRecordingButton.disabled = true;
-//     } catch (error) {
-//         console.error("Error starting recording:", error);
-//     }
-// }
-
-// startRecordingButton.addEventListener("click", startRecording);
-
-
-// const openRecorder = async () => {
-//     // Get the URL of the recorder page from the extension's resources
-//     const recorderUrl = chrome.runtime.getURL("recorder.html");
-    
-//     // Open the recorder page in a new tab
-//     const tab = await chrome.tabs.create({ url: recorderUrl });
-// };
-
-// startRecordingButton.addEventListener("click", openRecorder);
-
-
-
-
-
-
-
-
-//////////////////////////////////////////////////////
-
-
-
-// //  without the tab select
-// var btnStopRecording = document.getElementById("stopRecording");
-// var btnStartRecording = document.getElementById("startRecording");
-// var video = document.getElementById("recordedVideo");
-// var recorder;
-
-// btnStartRecording.onclick = function () {
-//     btnStartRecording.disabled = true;
-//     btnStopRecording.disabled = false;
-
-//     recorder = new RecordRTC_Extension();
-//     recorder.startRecording({
-//         enableScreen: true,
-//     });
-// };
-
-// btnStopRecording.onclick = function () {
-//     btnStartRecording.disabled = false;
-//     btnStopRecording.disabled = true;
-
-//     recorder.stopRecording(function (blob) {
-//         var url = URL.createObjectURL(blob);
-//         video.src = url;
-//     });
-// };
-
-
-
-
-
-
-
-
-
-// appending the sidebar 
-
-
+// // appending the sidebar 
 
 document.addEventListener("DOMContentLoaded",()=>{
     const StartButton = document.getElementById("startRecording")
@@ -112,15 +11,18 @@ document.addEventListener("DOMContentLoaded",()=>{
         else if (tab.url.indexOf('file') === 0) {
             StartButton.innerHTML="MindCue Can't Access local files"}
         else{
-            StartButton.addEventListener("click",()=>{
+            StartButton.addEventListener("click",async ()=>{
             chrome.tabs.sendMessage(
                 tabs[0].id,
-                {from :"settings",query:"inject_side_bar"}
+                {from :"settings",query:"inject_side_bar"},
+
             )
-            // close the window of the settings page 
+          // close the window of the settings page 
             window.close()
             })
-      
+    
             }
 })
 })
+
+ 
